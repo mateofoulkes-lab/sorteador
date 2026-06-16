9b31e41d87d28da917d06bcab8ad944fa3cf52c6
mateofoulkes-lab <mateofoulkes@gmail.com>
2026-06-16 10:06:58 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,35 @@
+const APP_VERSION = '2.0.0-firebase-pwa';
+const CACHE_NAME = `pingu3d-rifa-${APP_VERSION}`;
+const CORE_ASSETS = [
+  './',
+  './index.html',
+  './firebase-config.js',
+  './manifest.webmanifest',
+  './assets/rifa-bg.png',
+  './assets/icon-192.png',
+  './assets/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(CORE_ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  const req = event.request;
+  if (req.method !== 'GET') return;
+  event.respondWith(
+    caches.match(req).then(cached => cached || fetch(req).then(res => {
+      const copy = res.clone();
+      caches.open(CACHE_NAME).then(cache => cache.put(req, copy)).catch(() => {});
+      return res;
+    }).catch(() => cached))
+  );
+});
